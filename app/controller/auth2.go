dac186dc53e96cb0967b234c9814c1eb363c33f0
liujun <liujun@canway.net>
2023-09-19 14:12:29 +0800

    feat: init pro

---
+++ app/controller/auth2.go
@@ -0,0 +1,20 @@
+package controller
+
+import (
+	"auth2/utils/code"
+	"auth2/utils/httpResp"
+	"github.com/gin-gonic/gin"
+)
+
+type Auth struct{
+
+}
+
+func NewAuth()Auth{
+	return Auth{}
+}
+
+func (a *Auth) Authorize(c *gin.Context)*httpResp.Response{
+
+	return httpResp.HttpResp(code.Success, "用户名不存在",map[string]string{"msg":"time"})
+}
